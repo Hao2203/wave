b6380838c69cc0dd2055396e356a8e1d02e5be45
Hao2203 <516501366@qq.com>
2024-12-17 17:48:35 +0800

    update

--- wave-session/src/lib.rs
+++ wave-session/src/lib.rs
@@ -1,4 +1,30 @@
+use async_trait::async_trait;
+use bytes::Bytes;
+use futures_lite::stream::Boxed as BoxStream;
+
 pub mod error;
 
 #[cfg(test)]
 mod test;
+
+#[derive(Debug)]
+pub struct Session {
+    pub name: String,
+}
+
+impl Session {
+    pub fn new(name: String) -> Session {
+        Session { name }
+    }
+}
+
+#[async_trait]
+pub trait Manager {
+    type Error;
+    type TopicId;
+    async fn create(&self, name: String) -> Result<Self::TopicId, Self::Error>;
+
+    async fn publish(&self, topic: &Self::TopicId, bytes: Bytes) -> Result<(), Self::Error>;
+
+    async fn subscribe(&self, topic: &Self::TopicId) -> Result<BoxStream<Bytes>, Self::Error>;
+}
