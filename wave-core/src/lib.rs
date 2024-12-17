364336707363702aef69336f750c58b922fd4f7c
Hao2203 <516501366@qq.com>
2024-12-17 11:08:06 +0800

    update

--- wave-core/src/lib.rs
+++ wave-core/src/lib.rs
@@ -1,4 +1,20 @@
+use iroh::PublicKey;
+
+pub mod server;
 #[cfg(test)]
 mod test;
 
-pub mod server;
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Hash,
+    PartialOrd,
+    Ord,
+    serde::Serialize,
+    serde::Deserialize,
+    derive_more::From,
+)]
+pub struct Address(PublicKey);
